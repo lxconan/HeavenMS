3ba50c2c1c29a483f018451a34b605455c717e6f
Liu Xia <lxconan@gmail.com>
2019-10-22 22:23:16 +0800

    chron: migrate old project structure to gralde managed, standard java project layout.

--- launch.bat
+++ launch.bat
@@ -1,6 +1,6 @@
 @echo off
 @title HeavenMS
-set PATH=C:\Program Files\Java\jdk1.8.0_211\bin;%PATH%
-set CLASSPATH=.;dist\*
+set PATH=C:\Program Files\Java\jdk1.8.0_221\bin;%PATH%
+set CLASSPATH=.;dist\*;dist\libs\*;
 java -Xmx2048m -Dwzpath=wz\ net.server.Server
 pause
